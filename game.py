357d96b741bc6c3a4610f43d321dfa5866dccffa
Ojas Shirekar <ojas.shirekar@gmail.com>
2021-03-11 14:51:40 +0100

    updated payoff function logic

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import scipy.integrate as integrate
 import math
 
 "Original code snipper"
+
+
 # function payment = payoff(mu,sigma,p,pplus,pmin)
 # %Inputs: mu and sigma of the agent (or coalition),
 # %The p, pplus and pminus POU Tariff parameters
@@ -24,20 +26,22 @@ import math
 # payment = p*mu - sigma*(pplus+pmin)*bigphi;
 # end
 
-def payoff(mu,sigma,p,pplus,pmin,x):
+def payoff(mu, sigma, p, pplus, pmin):
     "Added an x value for the @x, this will be declared outside"
-    if pplus+pmin!=0:
+    if pplus + pmin != 0:
         ropt = pplus / (pplus + pmin)
     else:
         ropt = 0
-    fun = norm.ppf(x)
+    fun = norm.ppf
     bigphi = 0
-    if ropt!=0:
-        bigphi = integrate.quad(fun, 0, ropt)
+    if ropt != 0:
+        bigphi, _ = integrate.quad(fun, 0, ropt)
     return p * mu - sigma * (pplus + pmin) * bigphi
 
 
 "Original code snipper"
+
+
 # function sd=sigma_set(vect)
 # %Input: vector of individual sigmas
 # %Output: sigma of coalition
@@ -50,10 +54,8 @@ def payoff(mu,sigma,p,pplus,pmin,x):
 # end
 
 def sd(vect):
-
-    s= 0
-    for i in range(0,len(vect[0])):
+    s = 0
+    for i in range(0, len(vect[0])):
         s = s + vect(i) ^ 2
 
     return math.sqrt(s)
-
